a40c1f7ea8bdeb9bf9b3b6a489c68af17dd92adf
Raj Aryan <rajaryan.rsr@gmail.com>
2026-03-19 15:41:20 +0530

    Feat: MArketing strategy agent

---
+++ graph/state.py
@@ -0,0 +1,6 @@
+from langgraph.graph import MessagesState
+
+
+class AgentState(MessagesState):
+    """State class for agents to carry the coversation history and other relevant information."""
+    pass
